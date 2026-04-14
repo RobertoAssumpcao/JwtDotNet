72188c5e2779e967fa9c5ca30542a6373bf8c3e6
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-04-13 21:21:37 -0300

    enhance authentication and authorization in Program.cs with JWT support and role-based policies

--- JwtDotNet/Program.cs
+++ JwtDotNet/Program.cs
@@ -1,21 +1,39 @@
 using System.Security.Claims;
+using System.Text;
+using JwtDotNet;
 using JwtDotNet.Models;
 using JwtDotNet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<TokenService>();
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("Admin", p => p.RequireRole("admin"));
 
 var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/", (TokenService service) =>
+app.MapGet("/login", (TokenService service)
+    =>
 {
     var user = new User(
         1,
-        "teste",
+        "teste teste",
         "xyz@teste",
         "https://teste/",
         "xyxz",
@@ -23,5 +41,18 @@ app.MapGet("/", (TokenService service) =>
 
     return service.Create(user);
 });
+
+app.MapGet("/restrito", (ClaimsPrincipal user) => new
+    {
+        id = user.Id(),
+        name = user.Name(),
+        email = user.Email(),
+        givenName = user.GivenName(),
+        image = user.Image(),
+    })
+    .RequireAuthorization();
+
+app.MapGet("/admin", () => "Você tem acesso!")
+    .RequireAuthorization("admin");
 
 app.Run();
